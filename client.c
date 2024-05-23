f407541d24bce09188005bcc557a8348eba01f5b
dabochko <dabochko@student.42barcelona.com>
2024-05-23 14:38:00 +0200

    Implemented message transmission

--- client.c
+++ client.c
@@ -18,3 +18,26 @@ void	send_signal(int pid, unsigned char c)
 		usleep(100);
 	}
 }
+
+int	main(int argc, char *argv[])
+{
+	pid_t		server_pid;
+	const char	*message;
+	int			i;
+
+	if (argc != 3)
+	{
+		ft_printf("Usage: %s <pid> <message>\n", argv[0]);
+		exit(0);
+	}
+	server_pid = ft_atoi(argv[1]);
+	message = argv[2];
+	i = 0;
+	while (message[i])
+	{
+		send_signal(server_pid, message[i]);
+		i++;
+	}
+	send_signal(server_pid, '\0');
+	return (0);
+}
