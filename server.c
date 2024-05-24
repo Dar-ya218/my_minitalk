306fe43fa3ebc6b201eaf5734b1804b4bd67eb7c
dabochko <dabochko@student.42barcelona.com>
2024-05-24 12:23:10 +0200

    Implemented signal handling function

---
+++ server.c
@@ -0,0 +1,21 @@
+#include "minitalk.h"
+
+void	handle_signal(int signal)
+{
+	static unsigned char	current_char;
+	static int				bit_index;
+
+	current_char |= (signal == SIGUSR1);
+	bit_index++;
+	if (bit_index == 8)
+	{
+		if (current_char == '\0')
+			ft_printf("\n");
+		else
+			ft_printf("%c", current_char);
+		bit_index = 0;
+		current_char = 0;
+	}
+	else
+		current_char <<= 1;
+}
